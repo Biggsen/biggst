2aaae58bdc788f892b2838fbcfe015e1b6bad78a
Biggs Thorarensen <biggs@devillion.com>
2021-06-05 11:34:13 +0100

    using next/image for Profile. alt text added.

--- components/Profile.js
+++ components/Profile.js
@@ -1,10 +1,11 @@
 import styles from '../styles/Profile.module.css'
+import Image from 'next/image'
 
 const Profile = () => {
     return (
         <div>
             <p>I'm Biggs Thorarensen, a senior front end web developer based in London. I've had over 14 years of experience building for the web. I approach projects armed with a hawkeye for details, unwavering attention to quality and a passion for excellent user experience.</p>
-            <img src="/profile.jpeg" alt="" />
+            <Image src="/profile.jpeg" alt="Biggs Thorarensen, a full-bearded white male with glasses wearing a black and white checkered striped shirt." width={500} height={500} />
         </div>
     )
 }
