63ef10996af4b3ea03c58f5d3f6ebd7da67a9348
Biggs Thorarensen <biggs@devillion.com>
2021-06-06 18:51:48 +0100

    Testimonial styles

--- components/Testimonial.js
+++ components/Testimonial.js
@@ -1,9 +1,12 @@
+import styles from '../styles/Testimonial.module.css'
+
 const Testimonial = () => {
     return (
         <div>
-            <h3>What they say about me</h3>
-            <blockquote>"If you're looking for a world-class web developer then Biggs could well be your man. He's an absolute pleasure to work with and immediately understands not only the requirements of the project but also of the business as a whole.<br/><br/>Whilst he has a brilliant grasp of the technologies involved in web development he also manages to apply them pragmatically rather than with rigid dogma."</blockquote>
-            <p>Mark Wright, Executive Producer at R/GA</p>
+            <h3 className={styles.heading}>What they say about me</h3>
+            <blockquote className={styles.blockquote}>
+                "If you're looking for a world-class web developer then Biggs could well be your man. He's an absolute pleasure to work with and immediately understands not only the requirements of the project but also of the business as a whole.<br/><br/>Whilst he has a brilliant grasp of the technologies involved in web development he also manages to apply them pragmatically rather than with rigid dogma."</blockquote>
+            <p className={styles.paragraph}>Mark Wright, Executive Producer at R/GA</p>
         </div>
     )
 }
