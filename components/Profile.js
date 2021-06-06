79a7c50c7412143e3d169cb239957a0ccbc2b94e
Biggs Thorarensen <biggs@devillion.com>
2021-06-06 18:49:23 +0100

    paragraph styles for Profile text

--- components/Profile.js
+++ components/Profile.js
@@ -5,7 +5,7 @@ const Profile = () => {
     return (
         <div className={styles.container}>
             <div className={styles.content}>
-                <p>I'm Biggs Thorarensen, a senior front end web developer based in London. I've had over 15 years of experience building for the web. I approach projects armed with a hawkeye for details, unwavering attention to quality and a passion for excellent user experience.</p>
+                <p className={styles.paragraph}>I'm Biggs Thorarensen, a senior front end web developer based in London. I've had over 15 years of experience building for the web. I approach projects armed with a hawkeye for details, unwavering attention to quality and a passion for excellent user experience.</p>
             </div>
             <div className={styles.image}>
                 <Image src="/profile.jpeg" alt="Biggs Thorarensen, a full-bearded white male with glasses wearing a black and white checkered striped shirt." width={500} height={500} />
