1b3fac2f3f2cfc99df69377a01db66e838c0a105
Biggs Thorarensen <biggs@devillion.com>
2021-06-06 16:48:09 +0100

    Profile component styling

--- components/Profile.js
+++ components/Profile.js
@@ -4,8 +4,12 @@ import Image from 'next/image'
 const Profile = () => {
     return (
         <div className={styles.container}>
-            <p>I'm Biggs Thorarensen, a senior front end web developer based in London. I've had over 15 years of experience building for the web. I approach projects armed with a hawkeye for details, unwavering attention to quality and a passion for excellent user experience.</p>
-            <Image src="/profile.jpeg" alt="Biggs Thorarensen, a full-bearded white male with glasses wearing a black and white checkered striped shirt." width={500} height={500} />
+            <div className={styles.content}>
+                <p>I'm Biggs Thorarensen, a senior front end web developer based in London. I've had over 15 years of experience building for the web. I approach projects armed with a hawkeye for details, unwavering attention to quality and a passion for excellent user experience.</p>
+            </div>
+            <div className={styles.image}>
+                <Image src="/profile.jpeg" alt="Biggs Thorarensen, a full-bearded white male with glasses wearing a black and white checkered striped shirt." width={500} height={500} />
+            </div>
         </div>
     )
 }
