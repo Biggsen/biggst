76d45ffa2622115da6b3013f09b1bcae44cea095
Biggs Thorarensen <biggs@devillion.com>
2021-06-06 18:46:36 +0100

    Header component: using title and subtitle instead of h1 and h2. heading styles moved into here.

--- components/Header.js
+++ components/Header.js
@@ -3,9 +3,9 @@ import styles from '../styles/Header.module.css'
 const Header = ({title, subtitle}) => {
     return (
         <div>
-            <h1 className={styles.h1}>{title}</h1>
+            <h1 className={styles.title}>{title}</h1>
             {subtitle.length
-                ? <h2 className={styles.h2}>{subtitle}</h2>
+                ? <h2 className={styles.subtitle}>{subtitle}</h2>
                 : null
             }
         </div>
