273c1120c7ea9a697bd18896e02f3882e24241cf
Biggs Thorarensen <biggs@devillion.com>
2021-06-06 16:52:26 +0100

    Testimonial component added

---
+++ components/Testimonial.js
@@ -0,0 +1,11 @@
+const Testimonial = () => {
+    return (
+        <div>
+            <h3>What they say about me</h3>
+            <blockquote>"If you're looking for a world-class web developer then Biggs could well be your man. He's an absolute pleasure to work with and immediately understands not only the requirements of the project but also of the business as a whole.<br/><br/>Whilst he has a brilliant grasp of the technologies involved in web development he also manages to apply them pragmatically rather than with rigid dogma."</blockquote>
+            <p>Mark Wright, Executive Producer at R/GA</p>
+        </div>
+    )
+}
+
+export default Testimonial
