d62b4cef9999c2cd7bda0497b205d16597e0f8ca
Biggs <biggsy@pm.me>
2025-06-13 15:24:51 +0100

    profile updated

--- components/Profile.js
+++ components/Profile.js
@@ -5,10 +5,10 @@ const Profile = () => {
     return (
         <div className={styles.container}>
             <div className={styles.content}>
-                <p className={styles.paragraph}>I'm Biggs Thorarensen, a senior front end web developer based in London. I've had over 15 years of experience building for the web. I approach projects armed with a hawkeye for details, unwavering attention to quality and a passion for excellent user experience.</p>
+                <p className={styles.paragraph}>I'm Biggs Thorarensen, a senior front end web developer based in Eastbourne. I've had over 20 years of experience building for the web. I approach projects armed with a hawkeye for details, unwavering attention to quality and a passion for excellent user experience.</p>
             </div>
             <div className={styles.image}>
-                <Image src="/profile.jpeg" alt="Biggs Thorarensen, a full-bearded white male with glasses wearing a black and white checkered striped shirt." width={500} height={500} />
+                <Image src="/biggs.png" alt="Biggs Thorarensen, a full-bearded white male looking up at the sky with trees in the background" width={500} height={500} />
             </div>
         </div>
     )
